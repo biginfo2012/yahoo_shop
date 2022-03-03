c2f9a370d694892d3e3a02433cf74d4de3e90e9f
biginfo2012 <83626701+biginfo2012@users.noreply.github.com>
2022-03-03 09:13:52 +0800

    final

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,34 +12,60 @@
 
         <!-- Styles -->
         <!-- BEGIN: Vendor CSS-->
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}vendors.min.css">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/vendors.min.css')}}">
         <!-- END: Vendor CSS-->
 
         <!-- BEGIN: Theme CSS-->
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}bootstrap-extended.min.css">
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}colors.min.css">
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}components.min.css">
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}themes/dark-layout.min.css">
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}themes/bordered-layout.min.css">
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}themes/semi-dark-layout.min.css">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap-extended.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/colors.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/components.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/themes/dark-layout.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/themes/bordered-layout.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/themes/semi-dark-layout.min.css')}}">
 
         <!-- BEGIN: Page CSS-->
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}vertical-menu.min.css">
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}forms/form-validation.css">
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}authentication.css">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/vertical-menu.min.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/forms/form-validation.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/authentication.css')}}">
         <!-- END: Page CSS-->
 
         <!-- BEGIN: Custom CSS-->
-        <link rel="stylesheet" type="text/css" href="{{asset('css')}}style.css">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
         <!-- END: Custom CSS-->
 
         <!-- Scripts -->
         <script src="{{ asset('js/app-origin.js') }}" defer></script>
     </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
+    <body class="vertical-layout vertical-menu-modern blank-page navbar-floating footer-static  " data-open="click"
+          data-menu="vertical-menu-modern" data-col="blank-page">
+        <div class="app-content content ">
             {{ $slot }}
         </div>
+
+        <!-- BEGIN: Vendor JS-->
+        <script src="{{asset('js/vendors.min.js')}}"></script>
+        <!-- BEGIN Vendor JS-->
+
+        <!-- BEGIN: Page Vendor JS-->
+        <script src="{{asset('js/jquery.validate.min.js')}}"></script>
+        <!-- END: Page Vendor JS-->
+
+        <!-- BEGIN: Theme JS-->
+        <script src="{{asset('js/app-menu.min.js')}}"></script>
+        <script src="{{asset('js/app.min.js')}}"></script>
+        <!-- END: Theme JS-->
+
+        <!-- BEGIN: Page JS-->
+        <script src="{{asset('js/auth-login.js')}}"></script>
+        <!-- END: Page JS-->
+
+        <script>
+            $(window).on('load', function () {
+                if (feather) {
+                    feather.replace({width: 14, height: 14});
+                }
+            })
+        </script>
     </body>
 </html>
